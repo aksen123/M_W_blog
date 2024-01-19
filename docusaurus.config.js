a837b7b04aab9f414bc9ef148ba2237390bb861a
mw <minwook0523@gmail.com>
2024-01-19 19:06:21 +0900

    OG test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,34 +4,34 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'M_W',
-  tagline: ' ',
-  favicon: 'img/favicon.png',
-  url: 'https://aksen123.github.io',
-  baseUrl: '/M_W_blog',
-  organizationName: 'aksen123', // Usually your GitHub org/user name.
-  projectName: 'M_W_blog', // Usually your repo name.
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  title: "M_W",
+  tagline: " ",
+  favicon: "img/favicon.png",
+  url: "https://aksen123.github.io",
+  baseUrl: "/M_W_blog",
+  organizationName: "aksen123", // Usually your GitHub org/user name.
+  projectName: "M_W_blog", // Usually your repo name.
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -41,33 +41,34 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: '/img/ms.jpg',
-      description: 'test',
+      image: "/img/ms.jpg",
+      description: "test",
       metadata: [
-        {name: 'keywords', content: 'M_W, blog'},
-        {name: 'og:title', content: '내가만듦'},
-        {property: "og:image", content: 'https://aksen123.github.io/M_W_blog/img/ms.jpg'},
-        {property:'og:image:width', content: '800'},
-        {property:'og:image:height', content: '400'}
+        { name: "keywords", content: "M_W, blog" },
+        { name: "og:title", content: "내가만듦" },
+        {
+          property: "og:image",
+          content: "https://aksen123.github.io/M_W_blog/img/ms.jpg",
+        },
       ],
       navbar: {
-        title: '일단 기록하자',
+        title: "일단 기록하자",
         logo: {
-          alt: 'M_W Blog',
-          src: 'img/favicon2.png',
+          alt: "M_W Blog",
+          src: "img/favicon2.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: '끄적끄적',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "끄적끄적",
           },
-          {to: '/docs/tags', label: 'Tags', position: 'left'},
+          { to: "/docs/tags", label: "Tags", position: "left" },
           {
-            href: 'https://github.com/aksen123',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/aksen123",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
